9534e14c7f19a1582c8c5846dfcdefe4e9ace08f
Xavier Laviron <xavier.laviron@gmx.fr>
2020-10-17 22:53:37 +0200

    adds registration form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use App\Repository\UserRepository;
+use App\Security\LoginFormAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -32,5 +41,59 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/registration", name="user_registration")
+     */
+    public function registrate(
+        Request $request,
+        UserPasswordEncoderInterface $passwordEncoder,
+        EntityManagerInterface $em,
+        UserRepository $repo,
+        GuardAuthenticatorHandler $quardHandler,
+        LoginFormAuthenticator $formAuthenticator
+    ) {
+        // TODO: check user password
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = $form->get('email')->getData();
+            if ($repo->findOneBy(['email' => $email])) {
+                // chekc if email is already used
+                $form->addError(new FormError("The email $email is already used."));
+
+                return $this->render('user/registration.html.twig', [
+                    'form' => $form->createView(),
+                ]);
+            }
+
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $user->setCreatedAt(\date_create());
+            $user->setRoles(['ROLE_CONTRIBUTOR']);
+
+            $em->persist($user);
+            $em->flush();
+
+            // login and redirect to the target page
+            return $quardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $formAuthenticator,
+                'main'
+            );
+        }
+
+        return $this->render('user/registration.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
